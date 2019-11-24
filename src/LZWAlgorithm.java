dba7b11f3fdc9423bc397667d2839d6a02ca1281
2191867E <jasonegan59@gmail.com>
2019-11-24 17:29:26 +0000

    class restructure complete, needs refactoring

--- src/LZWAlgorithm.java
+++ src/LZWAlgorithm.java
@@ -1,6 +1,5 @@
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -24,6 +23,7 @@ public class LZWAlgorithm {
     public void executeAlgorithm() throws IOException {
         initialiseTrie();
         readFromFile();
+        addWordsToTrie();
     }
 
     public void initialiseTrie() {
@@ -60,6 +60,57 @@ public class LZWAlgorithm {
 
         }
         reader.close();
+    }
+
+    public void addWordsToTrie() {
+        ArrayList<Character> chars = getChars();
+        int i = 0;
+        String s;
+
+        //start the count from 128 as the ASCII chars are already in the trie
+        long count = 128;
+
+        //codeWordLength is now 8 as 2^7 chars are already in the trie
+        int codeWordLength = 8;
+
+        //whilst the position is within index of the chars array list
+        while (i < chars.size()) {
+
+            //get the current char from the given index i
+            s = String.valueOf(chars.get(i));
+
+            //while loop to get the largest string s in the trie
+            while ((i + s.length() < chars.size()) && getTrie().search(s)) {
+
+                //appends the new char to s if it is already in the trie
+                s = s + String.valueOf(chars.get(i + s.length()));
+            }
+
+            //increments i by the length of s
+            i += s.length();
+
+            //decrements i if the length is greater than 1 to go back a char
+            if (s.length() > 1) {
+                i = i - 1;
+            }
+
+            if (i < chars.size() && !getTrie().search(s)) {
+
+                //inserts new string s to trie, increments count, and adds the new codeWordLength to the codeCount
+                getTrie().insert(s);
+                count++;
+                setCodeCount(getCodeCount() + codeWordLength);
+            }
+
+
+            //checks whether the codeWordLength needs increased to double dictionary space
+            if (count > Math.pow(2, codeWordLength)) {
+                codeWordLength += 1;
+            }
+        }
+
+        //adds the final codeWord of the file
+        setCodeCount(getCodeCount() + codeWordLength);
     }
 
     public Trie getTrie() {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.*;
-
 
 /**
  * program to find compression ratio using LZW compression
@@ -11,56 +9,6 @@ public class Main {
         long start = System.currentTimeMillis();
         String inputFileName = args[0];
         LZWAlgorithm lzw = new LZWAlgorithm(inputFileName);
-
-        ArrayList<Character> chars = lzw.getChars();
-        int i = 0;
-        String s = "";
-
-        //start the count from 128 as the ASCII chars are already in the trie
-        long count = 128;
-
-        //codeWordLength is now 8 as 2^7 chars are already in the trie
-        int codeWordLength = 8;
-
-        //whilst the position is within index of the chars array list
-        while (i < chars.size()) {
-
-            //get the current char from the given index i
-            s = String.valueOf(chars.get(i));
-
-            //while loop to get the largest string s in the trie
-            while ((i + s.length() < chars.size()) && lzw.getTrie().search(s)) {
-
-                //appends the new char to s if it is already in the trie
-                s = s + String.valueOf(chars.get(i + s.length()));
-            }
-
-            //increments i by the length of s
-            i += s.length();
-
-            //decrements i if the length is greater than 1 to go back a char
-            if (s.length() > 1) {
-                i = i - 1;
-            }
-
-            if (i < chars.size() && !lzw.getTrie().search(s)) {
-
-                //inserts new string s to trie, increments count, and adds the new codeWordLength to the codeCount
-                lzw.getTrie().insert(s);
-                count++;
-                lzw.setCodeCount(lzw.getCodeCount() + codeWordLength);
-            }
-
-
-            //checks whether the codeWordLength needs increased to double dictionary space
-            if (count > Math.pow(2, codeWordLength)) {
-                codeWordLength += 1;
-            }
-        }
-
-        //adds the final codeWord of the file
-        lzw.setCodeCount(lzw.getCodeCount() + codeWordLength);
-
 
         //closes resources
         String outputFileName = args[1];
